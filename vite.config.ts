e12058af664b490fed1fe1beae0c84fa584a8d81
sinnn9593 <sinnn9593@gmail.com>
2025-02-23 16:24:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // または '/' にする
+  base: '/no1/', // または '/' にする
   plugins: [react(),tailwindcss()],
 })
