8ba9b1f4e55d7e96d9f291f8bc7340d10dc7ca9e
soji-kodama <soji.kodama@epkotsoftware.co.jp>
2025-02-28 00:44:42 +0900

    change the way to do deploy

--- deploy.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from "child_process";
-
-try {
-  execSync("git add dist -f && git commit -m 'Deploy' && git subtree push --prefix dist origin gh-pages", { stdio: "inherit" });
-  console.log("✅ デプロイ完了");
-} catch (error) {
-  console.error("❌ デプロイエラー", error);
-}
